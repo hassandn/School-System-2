53bfe0a1b20b27d5a0e87d9d36421717e9fda532
hassandn <hassandn6350@gmail.com>
2025-04-21 13:50:36 +0330

    refactor helper to class base

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -2,24 +2,47 @@ from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from rest_framework.exceptions import ValidationError
 
-User = get_user_model()
-def create_user(username, password, first_name, last_name, national_id, email, group_name):
-    try:
-        print(password)
-        user = User.objects.create_user(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            national_id=national_id,
-            registration_status='pending',
-            email=email,
-        )
-        user.set_password(password)
-        group = Group.objects.get(name=str(group_name))
-        group.user_set.add(user)
-        user.save() 
-        return user
-    except group.DoesNotExist:
-        raise ValidationError(f"Group '{group_name}' does not exist.")
-    except Exception as e:
-        raise ValidationError(f"Error creating user: {str(e)}")
+class UserManager:
+    def __init__(self, username, password, first_name, last_name, national_id, email, group_names):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.national_id = national_id
+        self.email = email
+        self.group_names = group_names
+        
+    def create_user(self):
+        try:
+            user = get_user_model().objects.create_user(
+                username=self.username,
+                first_name=self.first_name,
+                last_name=self.last_name,
+                national_id=self.national_id,
+                registration_status='pending',
+                email=self.email,
+            )
+            user.set_password(self.password)
+            for user_group in self.group_names:
+                group = Group.objects.get(name=str(user_group))
+                # group = Group.objects.get(name=str(self.group_names))
+                group.user_set.add(user)
+            
+            user.save() 
+            return user
+        except Group.DoesNotExist:
+            raise ValidationError(f"Group '{self.group_names}' does not exist.")
+        except Exception as e:
+            raise ValidationError(f"Error creating user: {str(e)}")
+        
+    def update_user(username, first_name, last_name, location, email, group_names):
+        try:
+            get_user_model().objects.update(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                location=location,
+                email=email,
+            )
+        except Exception as e:
+            pass
