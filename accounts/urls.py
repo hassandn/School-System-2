06c774038a5de749707be582315f80f4616166c2
hassandn <hassandn6350@gmail.com>
2025-04-26 15:26:01 +0330

    config jwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import UserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
     path('create-user/', UserView.as_view(), name='user_create'),
     path('list-users/', UserView.as_view(), name='user_lsit'),
     path('update-user/<int:pk>/', UserView.as_view(), name='user_update'),
