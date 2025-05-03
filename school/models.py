d285694b0128f3202bb32df684cc82a5d02c82f0
hassandn <hassandn6350@gmail.com>
2025-05-03 16:17:28 +0330

    create school app and implement school course and class models

---
+++ school/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.auth import get_user_model
+
+
+class School(models.Model):
+    school_name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    location = models.PointField( blank=False, null=False)
+    
+    def __str__(self):
+        return f"{self.school_name}"
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    credits = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return f"{self.course_name} - {self.credits}"
+    
+class Class(models.Model):
+    class_name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    couse = models.ForeignKey(to=Course, on_delete=models.PROTECT)
+    teacher_name = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, related_name='teacher_class')
+    students = models.ManyToManyField(to=get_user_model(), blank=True, related_name='student_class')
+    
+    def __str__(self):
+        return f"{self.class_name} - {self.couse} - {self.teacher_name}"
+    
+    
+
+    
