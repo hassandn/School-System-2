d5a7eeec58510d3fcc97422ae49549b73c5a8659
hassandn <hassandn6350@gmail.com>
2025-05-03 12:54:14 +0330

    remove old urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import UserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'users', UserView)
+
 
 urlpatterns = [
     path('token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
-    path('create-user/', UserView.as_view(), name='user_create'),
-    path('list-users/', UserView.as_view(), name='user_lsit'),
-    path('update-user/<int:pk>/', UserView.as_view(), name='user_update'),
-    path('user/<int:pk>/', UserView.as_view(), name="user_detail"),
-    path('delete/user/<int:pk>/', UserView.as_view(), name="user_delete"),
 ]
+urlpatterns += router.urls
