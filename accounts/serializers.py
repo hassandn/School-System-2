b609b9c5da8ddc31c7053048fdaf1cf1eb5f7471
hassandn <hassandn6350@gmail.com>
2025-05-04 14:42:01 +0330

    change password validator and make it better! ;)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,38 +1,14 @@
-from django.contrib.auth import get_user_model 
+from traceback import print_tb
+
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.db.models.fields import return_None
 from rest_framework import serializers
 from .helpers import UserManager
 import re
 
-def check_password_strength(data):#check if the password is strong enough
-    
-        if len(data['password']) < 8:#check if the password is strong enough
-            raise serializers.ValidationError("Password must be at least 8 characters long.")
-        
-        if not any(char.isdigit() for char in data['password']):#check if the password contains at least one digit 
-            raise serializers.ValidationError("Password must contain at least one digit.")     
-        
-        if not any(char.isupper() for char in data['password']):#check if the password contains at least one uppsercase Letter
-            raise serializers.ValidationError("Password must contain at least one uppercase letter.")
-        
-        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]",data['password']):#check if the password contains at least one characters
-            raise serializers.ValidationError("Password must contain at least one special character.")
-        if 'username' in data:
-            if data['username'].lower() in data['password'].lower():#check if the password not have username in it
-                raise serializers.ValidationError("Password must not contain username.")
-        else:
-            raise serializers.ValidationError("you didn't fill usename")
-        if 'email' in data:
-            if data['email'].lower().split('@')[0] in data['password'].lower():#check if the password not have email in it
-                raise serializers.ValidationError("Password must not contain email.")
-        else:
-            raise serializers.ValidationError("you didn't fill email")
-        if 'national_id' in data:
-            if data['national_id'].lower() in data['password'].lower():#check if the password contains national id
-                raise serializers.ValidarionError("password must not contain national id.")
-        else:
-            raise serializers.ValidationError("you didn't fill national id")
-      
+
 class UserSerializer(serializers.ModelSerializer):
     """serilizer for validate show and create user"""
     groups = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all(), many=True)
@@ -45,18 +21,44 @@ class UserSerializer(serializers.ModelSerializer):
         user = UserManager(validated_data['username'], validated_data['password'], validated_data['first_name'], validated_data['last_name'], validated_data['national_id'], validated_data['email'] , validated_data['groups'])
         user = user.create_user()
         return user
-    
-    def validate(self, data):
-        if 'password' in data:
-            check_password_strength(data)
-        if 'national_id' in data:
-            if any(char.isalpha() for char in data['national_id']):#check if national id has characters
-                raise serializers.ValidationError("National ID must contain only digits.")
-        if all(field  in data for field in self.fields):
-            return data 
+
+    def validate_national_id(self, value):
+        if any(char.isalpha() for char in value):  # check if national id has characters
+            raise serializers.ValidationError("National ID must contain only digits.")
+        return value
+
+    def validate_password(self, value):# check if the password is strong enough
+        if len(value) < 8:  # check if the password is strong enough
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+
+        if not any(char.isdigit() for char in value):  # check if the password contains at least one digit
+            raise serializers.ValidationError("Password must contain at least one digit.")
+
+        if not any(char.isupper() for char in value):  # check if the password contains at least one uppsercase Letter
+            raise serializers.ValidationError("Password must contain at least one uppercase letter.")
+
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):  # check if the password contains at least one characters
+            raise serializers.ValidationError("Password must contain at least one special character.")
+
+        if 'username' in self.initial_data:
+            if self.initial_data['username'].lower() in value.lower():  # check if the password not have username in it
+                raise serializers.ValidationError("Password must not contain username.")
         else:
-            raise serializers.ValidationError("you didn't fill all fields")
-    
+            raise serializers.ValidationError("you didn't fill usename")
+
+        if 'email' in self.initial_data:
+            if self.initial_data['email'].lower().split('@')[0] in value.lower():  # check if the password not have email in it
+                raise serializers.ValidationError("Password must not contain email.")
+        else:
+            raise serializers.ValidationError("you didn't fill email")
+
+        if 'national_id' in self.initial_data:
+            if self.initial_data['national_id'].lower() in value.lower():  # check if the password contains national id
+                raise serializers.ValidationError("password must not contain national id.")
+        else:
+            raise serializers.ValidationError("you didn't fill national id")
+        return value
+
     def to_representation(self, instance):
         return UserManager.to_representation(user=super().to_representation(instance))
     
