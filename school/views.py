99498a75b8ba9a6503a03fc2255ff530e7c3a977
hassandn <hassandn6350@gmail.com>
2025-05-05 16:07:54 +0330

    implement api for school CRUD

--- school/views.py
+++ school/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsAdminPermission
+from .models import School
+from .serializers import SchoolSerializer
 
-# Create your views here.
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = SchoolSerializer
+    queryset = School.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'create']:
+            self.permission_classes = [IsAdminPermission]
+        return super(SchoolViewSet, self).get_permissions()
