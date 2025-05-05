fe774a9936b6cf305e23d3233850febdf3a3c108
hassandn <hassandn6350@gmail.com>
2025-05-05 16:10:25 +0330

    nothing changed in permissions

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -3,3 +3,10 @@ from rest_framework.permissions import BasePermission
 class IsOwnerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.pk == request.user.pk
+
+class IsAdminPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='Admin').exists()
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.groups.filter(name='Admin').exists()
